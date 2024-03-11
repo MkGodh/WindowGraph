9e8c3a68029c1411112369dac2e24ce0014d3227
MkGodh <fenryrvla@yandex.ru>
2024-03-11 15:37:10 +0300

    Добавьте файлы проекта.

---
+++ WindowGraph/WindowGraph.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+ setlocale(LC_ALL, "Rus");
+
+ int width, height; 
+ cout << "Введите ширину: "; cin >> width;
+ cout << "Введите высоту: "; cin >> height;
+ for (int row = 1; row <= height; row++) {
+     for (int col = 0; col <= width; col++) {
+         if (col == 1 || col == width) {
+             cout << "|";
+         }
+         else if (col >= 1 && col < width && row == 1 || col >= 1 && col < width && row == height) {
+             cout << "-";
+         }
+         else {
+             cout << " ";
+         }
+     }
+     cout << "\n";
+ }
+}
+
